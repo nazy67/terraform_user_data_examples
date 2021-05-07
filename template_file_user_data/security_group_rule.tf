9bb4d8dd738a14260893ff704e4b961c8f580e5f
nazy67 <nazykh67@gmail.com>
2021-05-06 22:44:09 -0500

    added security_group_rule.tf

---
+++ template_file_user_data/security_group_rule.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow TLS inbound traffic"
+  tags = {
+    Name = "web_sg"
+  }
+}
+resource "aws_security_group_rule" "http_ingress" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+}
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+}
